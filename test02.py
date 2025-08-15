d438c5b91319e6fd4a42721858bee3de1dd4afa7
Paphitchaya Bunyong <nanonisno@gmail.com>
2025-08-15 11:34:01 +0700

    ลงงานใหม่V1

--- test02.py
+++
@@ -1,6 +0,0 @@
-# คำสั่งที่แสดงผลใดๆ ทางหน้าจอใฃ้คำสั่ง  ฟังก์ชัน print()
-# string
-print('hello...')
-# numner
-print(12345)   #integer/int
-print(45444.4444)  #float
--- test03.py
+++
@@ -1,4 +0,0 @@
-print("Hello")
-print("Hi....")
-print("123") ; print('bbbb')
-print(1234)
