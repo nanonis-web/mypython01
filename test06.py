38773bf2009c0e441a5bafab0341d7eff592c8da
Paphitchaya Bunyong <nanonisno@gmail.com>
2025-08-22 12:28:03 +0700

    Add files via upload

---
+++ test06.py
@@ -0,0 +1,32 @@
+# Operrater ตัวดำเนินการ คือ เครื่องหมายที่ใช้ในการประมวณผลข้อมูล
+# 1. Arithmetic Operrater  เครื่องหมายคำนวน
+# (+,-,*./,% mod,//, floor,**eponential)
+print(10+3)
+print(10-3)
+print(10*3)
+print(10/3)
+print(10%3) #หารเอาเศษ
+print(10//3) #หารเอาส่วน
+print(10**3) 
+print("+++++++++++++++++++++++") 
+# 2. Comparison Operrater  เครื่องหมายเปรียบเทียบ (==,!=,>,<,>=,<=)
+# ผลลัพธ์ที่ได้ มีแค่ True/False
+print(10 == 3)
+print(10 != 3)
+print('Bangna' >= 'Bangpoo')
+print('Thailand' < 'THAILAND')
+print('++++++++++++++++++++++')
+# 3. Logical Operrater  เครื่องหมายตรรกะ not,and,or
+# ข้อมูลที่จะใช้กับเครื่องหมายนี้จะต้องเป็น True/False และผลลัพธ์ที่จะได้จากการใช้เครื่องหมาย True/ False
+print(not True)
+print(not False)
+print('----------')
+print(True and True)
+print(True and False)
+print(False and False)
+print(False and True)
+print('----------')
+print(True or True)
+print(True or False)
+print(False or True)
+print(False or False) #False
