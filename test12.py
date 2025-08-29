741771e0f69b45fb375d97fd6d012d47a7919e01
Paphitchaya Bunyong <nanonisno@gmail.com>
2025-08-29 11:46:48 +0700

    Add files via upload

---
+++ test12.py
@@ -0,0 +1,13 @@
+# สำหรับ Python สโคปการทำงานหนึ่งที่เราใช้ indent/่ย่อหน้า/เยื้อง(กดปุ่ม Tab)
+
+# คำสั่ง if
+
+x = 10 
+if x <= 10 : #colon อย่าลืม
+    print('hello....')
+    print('Hi.....')
+    print('Hey....')
+
+    print('Bye bye ...')
+    print('Ei..ei...')
+     
---
+++ test13.py
@@ -0,0 +1,15 @@
+# คำสั่ง if,else
+
+x = 20 + 10
+
+if x <= 10 : #colon อย่าลืม
+    print('hello....')
+    print('Hi.....')
+    print('Hey....')
+else : #colon อย่าลืม 
+    print('Wow ...')
+    print('Woo...')
+    print('Wee...')
+
+print('Bye...Bye')
+print('Ei ei....')
